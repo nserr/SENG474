4ba89d2cefa9f15e67a215cb9bc0423e9b5e4753
Noah Serr <noah.serr8@gmail.com>
2021-02-27 17:40:07 -0800

    added k-fold cross validation.

--- A2/logisticregression.py
+++ A2/logisticregression.py
@@ -27,14 +27,17 @@ def init():
     train_len = len(X_train)
     test_len = len(X_test)
 
+    sandal_count = 0
+    sneaker_count = 0
+
     # Convert training data to binary classification problem.
     for i in range(train_len):
 
-        if y_train[i] == 5:
+        if y_train[i] == 5 and sandal_count < 1000:
             y_train_temp.append(0)
             X_train_temp.append(X_train[i])
 
-        if y_train[i] == 7:
+        if y_train[i] == 7 and sneaker_count < 1000:
             y_train_temp.append(1)
             X_train_temp.append(X_train[i])
 
@@ -58,7 +61,7 @@ def init():
 
 
 # Implementation of logistic regression.
-def log_reg(X_train, y_train, X_test, y_test, c):
+def logistic_regression(X_train, y_train, X_test, y_test, c):
     clf = LogisticRegression(penalty = 'l2', max_iter = 1000000, C = c)
     clf.fit(X_train, y_train)
 
@@ -67,6 +70,79 @@ def log_reg(X_train, y_train, X_test, y_test, c):
     test_accuracy = metrics.accuracy_score(y_test, y_test_prediction)
 
     return train_accuracy, test_accuracy
+
+
+# Run logistic regression.
+def lr_standard():
+    X_train, y_train, X_test, y_test = init()
+    c_values = generate_c_values(0.000001, 10)
+
+    train_accuracies = []
+    test_accuracies = []
+
+    for c in c_values:
+        train_accuracy, test_accuracy = logistic_regression(X_train, y_train, X_test, y_test, c)
+        train_accuracies.append(train_accuracy)
+        test_accuracies.append(test_accuracy)
+
+    generate_plot(train_accuracies, test_accuracies, c_values)
+
+
+# Implementation of logistic regression with k-fold cross validation.
+def lr_kfold():
+    X_train, y_train, X_test, y_test = init()
+    
+    c_values = generate_c_values(0.000001, 10)
+    k_values = [5, 6, 7, 8, 9, 10]
+
+    test_accuracies = []
+
+    for k in k_values:
+        print("k: " + str(k))
+        fold_accuracies = []
+        
+        X_length = len(X_train)
+        y_length = len(y_train)
+        fold = X_length / k
+
+        for c in c_values:
+            print("\tc: " + str(c))
+            c_accuracies = []
+
+            for group in range(k):
+                next_group = group + 1
+                start = int(fold * group)
+                stop = int(fold * next_group)
+
+                if group == (k - 1):
+                    X_train_group = X_train[0 : start]
+                    y_train_group = y_train[0 : start]
+                
+                elif group == 0:
+                    X_train_group = X_train[stop + 1 : X_length]
+                    y_train_group = y_train[stop + 1 : y_length]
+
+                else:
+                    X_train_group = np.concatenate((
+                        np.array(X_train[0 : start]),
+                        np.array(X_train[stop + 1 : X_length])))
+                    
+                    y_train_group = np.concatenate((
+                        np.array(y_train[0 : start]),
+                        np.array(y_train[stop + 1 : y_length])))
+                
+                X_train_test = X_train[start : stop]
+                y_train_test = y_train[start : stop]
+
+                train_accuracy, test_accuracy = logistic_regression(X_train_group, y_train_group, X_train_test, y_train_test, c)
+                c_accuracies.append(test_accuracy)
+            
+            avg_accuracy = np.average(c_accuracies)
+            fold_accuracies.append(avg_accuracy)
+
+        test_accuracies.append(fold_accuracies)
+
+    generate_kfold_plot(test_accuracies, c_values)
 
 
 # Generates regulization parameters using a logarithmically spaced grid.
@@ -86,8 +162,8 @@ def generate_plot(train_accuracies, test_accuracies, c_values):
     plt.xscale('log')
     plt.ylabel("Accuracy")
 
-    plt.plot(c_values, train_accuracies, 'r', label = "Train Accuracy")
-    plt.plot(c_values, test_accuracies, 'b', label = "Test Accuracy")
+    plt.plot(c_values, train_accuracies, 'b', label = "Train Accuracy")
+    plt.plot(c_values, test_accuracies, 'g', label = "Test Accuracy")
 
     plt.legend()
 
@@ -96,19 +172,30 @@ def generate_plot(train_accuracies, test_accuracies, c_values):
     plt.clf()
 
 
+# Use matplotlib to create graph for results (with k-fold cross validation).
+def generate_kfold_plot(test_accuracies, c_values):
+    plt.title("Logistic Regression with K-Fold Cross Validation")
+    plt.xlabel("Regularization Parameter")
+    plt.xscale('log')
+    plt.ylabel("Accuracy")
+
+    plt.plot(c_values, test_accuracies[0], 'b', label = "k = 5")
+    plt.plot(c_values, test_accuracies[1], 'g', label = "k = 6")
+    plt.plot(c_values, test_accuracies[2], 'r', label = "k = 7")
+    plt.plot(c_values, test_accuracies[3], 'c', label = "k = 8")
+    plt.plot(c_values, test_accuracies[4], 'm', label = "k = 9")
+    plt.plot(c_values, test_accuracies[5], 'y', label = "k = 10")
+
+    plt.legend()
+
+    filename = "LogisticRegression_KFold.png"
+    plt.savefig(filename)
+    plt.clf()
+
+
 def main():
-    X_train, y_train, X_test, y_test = init()
-    c_values = generate_c_values(0.000001, 10)
+    lr_standard()
+    lr_kfold()
 
-    train_accuracies = []
-    test_accuracies = []
-
-    for c in c_values:
-        train_accuracy, test_accuracy = log_reg(X_train, y_train, X_test, y_test, c)
-        train_accuracies.append(train_accuracy)
-        test_accuracies.append(test_accuracy)
-
-    generate_plot(train_accuracies, test_accuracies, c_values)
-    
 
 main()
